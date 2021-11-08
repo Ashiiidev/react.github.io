b64f733281adf2e1f7b6b90bc8f969fe04e5ccd7
Ashiiidev <87383186+Ashiiidev@users.noreply.github.com>
2021-11-08 09:25:39 +0530

    t

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+
+import Nabar from './Nabar';
+import Shposi from './Shposi';
+
+
+function App() {
+  return (
+  <>
+  
+     <Nabar/>
+   <div className="container">
+   <Shposi/>
+    </div>
+ </>
+  );
+}
+
+export default App;
---
+++ src/Shposi.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shposi = () => {
+    return (
+        <div >
+         <h1  class="text-warning"> Android devlopent team </h1>
+          <button class="    btn-danger mx-2">+</button>
+            Add this item to Cart
+            <button class="  btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+export default Shposi
